c972f22d3de1da8900413bff82723cf2870584b7
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-03 10:32:53 -0600

    Added component for navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,17 @@
+<script>
+  const navItems = [
+    {name: 'Home', href: '/', styling: "text-red-500 hover:text-purple-500"},
+    {name: 'Price', href: '/', styling: "text-red-500 hover:text-purple-500"},
+    {name: 'About', href: '/about', styling: "text-red-500 hover:text-purple-500"},
+    {name: 'Contact', href: '/', styling: "text-red-500 hover:text-purple-500"}
+  ];
+</script>
+
+<header class="flex justify-between m-5">
+  <div>Logo</div>
+  <ul class="flex gap-5">
+    {#each navItems as {name, href, styling}}
+    <li><a {href} {styling}> {name} </a></li>
+    {/each}
+  </ul>
+</header>
