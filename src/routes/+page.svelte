81dc61f113514a07f8a902f24932172418cd7889
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-03 10:33:18 -0600

    Added navbar component to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script>
-  import Card from "../lib/components/card.svelte";
+  import Card from "../lib/components/Card.svelte";
+  import NavBar from "../lib/components/NavBar.svelte";
+  import Layout from "./+layout.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Card title="Money" content="Mr Crabs" />
-
-<Card title="Sandy" content="Squirrel" />
+<NavBar />
