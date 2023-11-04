98ae077908a360c5b4ceaaceba7174c511dddcb7
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-04 12:18:05 -0600

    placeholder for main page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
-  import Card from "../lib/components/Card.svelte";
-  import NavBar from "../lib/components/NavBar.svelte";
-  import Layout from "./+layout.svelte";
 </script>
 
-<NavBar />
+<section class="max-h-screen bg-amber-200">
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-6xl font-bold text-gray-900">Hello World</h1>
+    <p class="mt-5 text-xl text-gray-600">Welcome to your new Svelte app</p>
+  </div>
+</section>
+
+<section class="max-h-screen bg-amber-200">
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-6xl font-bold text-gray-900">Hello World</h1>
+    <p class="mt-5 text-xl text-gray-600">Welcome to your new Svelte app</p>
+  </div>
+</section>
+
+<div></div>
+
