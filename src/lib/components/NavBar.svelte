7d3d61f3529bcb08025a4506ee065ddfa9d421f6
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-03 23:08:07 -0600

    Styling navbar component

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,17 +1,71 @@
 <script>
-  const navItems = [
-    {name: 'Home', href: '/', styling: "text-red-500 hover:text-purple-500"},
-    {name: 'Price', href: '/', styling: "text-red-500 hover:text-purple-500"},
-    {name: 'About', href: '/about', styling: "text-red-500 hover:text-purple-500"},
-    {name: 'Contact', href: '/', styling: "text-red-500 hover:text-purple-500"}
-  ];
 </script>
 
-<header class="flex justify-between m-5">
-  <div>Logo</div>
-  <ul class="flex gap-5">
-    {#each navItems as {name, href, styling}}
-    <li><a {href} {styling}> {name} </a></li>
-    {/each}
+
+<nav class="navbar">
+  <h1><a href="/">Logo</a></h1>
+  <ul>
+    <li><a href="/">Dashboard</a></li>
+    <li><a href="/messages">Messages</a></li>
+    <li><a href="/calendar">Calendar</a></li>
+    <p>Recruitment</p>
+    <li><a href="/jobs">Jobs</a></li>
+    <li><a href="/candidate">Candidates</a></li>
+    <li><a href="/referrals">Referrals</a></li>
+    <li><a href="/careers">Career Site</a></li>
+    <p>Organization</p>
+    <li><a href="/employees">Employees</a></li>
+    <li><a href="/documents">Documents</a></li>
+    <li><a href="/reports">Reports</a></li>
   </ul>
-</header>
+</nav>
+
+  <style>
+    :root{
+      --bb: 2px solid rgb(184, 178, 178);
+      --gradient: linear-gradient(to right, #ff416c, #ff4b2b);
+    }
+    .navbar{
+      box-sizing: border-box;
+      width: 20%;
+      background: slategrey;
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+      position: fixed;
+    }
+
+    nav ul{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      height: 100%;
+      list-style: none;
+      padding: 20px;
+    }
+    nav h1{
+      color: white;
+      font-size: 1.5rem;
+      padding: 1rem;
+      margin: 0;
+      border-bottom: var(--bb);
+      text-align: center;
+    }
+
+    nav h1:hover{
+      color: rgb(184, 178, 178);
+      transition: 300ms ease-in-out;
+    }
+
+    nav ul li {
+     padding: 1rem;
+    }
+    nav ul li:hover{
+      background: var(--gradient);
+      border-radius: 5px;
+    }
+    nav ul p{
+      opacity: 20%;
+      cursor: default;
+    }
+  </style>
