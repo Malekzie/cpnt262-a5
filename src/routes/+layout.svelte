633f49e56931395d0c23bdc37a7d65e41644ef22
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-04 23:18:16 -0600

    Imported Searchbar to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
     import "../app.css";
     import NavBar from "../lib/components/NavBar.svelte";
+  import SearchBar from "../lib/components/SearchBar.svelte";
   </script>
  
  <NavBar />
-
+<SearchBar />
  <div class="margin">
    <slot />
  </div>
