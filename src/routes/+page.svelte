02b3ffadbd96fd8c7359a53ddca4cf239e190350
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-04 23:17:32 -0600

    Added placeholder content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,12 @@
 <script>
+  let User = "Admin";
+  let welcome = "Welcome to your dashboard. Please select what you are about to do today from the menu on the left.";
 </script>
 
-<section class="max-h-screen bg-amber-200">
+<section class="max-h-screen cursor-default bg-amber-200">
   <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-6xl font-bold text-gray-900">Hello World</h1>
-    <p class="mt-5 text-xl text-gray-600">Welcome to your new Svelte app</p>
-  </div>
-</section>
-
-<section class="max-h-screen bg-amber-200">
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-6xl font-bold text-gray-900">Hello World</h1>
-    <p class="mt-5 text-xl text-gray-600">Welcome to your new Svelte app</p>
+    <h1 class="text-6xl font-bold text-gray-900">Hello {User}</h1>
+    <p class="mt-5 text-xl text-gray-600">{welcome}</p>
   </div>
 </section>
 
