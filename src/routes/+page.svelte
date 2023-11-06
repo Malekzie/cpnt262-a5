bcd43a4e65996405fc1402af8e45baac2ae4e9a4
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-05 20:22:07 -0700

    Added default msg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
   let User = "Admin";
   let welcome = "Welcome to your dashboard. Please select what you are about to do today from the menu on the left.";
+  let downMessage = "Recruitment and Organization are still under development."
 </script>
 
 <section class="max-h-screen cursor-default bg-amber-200">
   <div class="flex flex-col items-center justify-center h-screen">
     <h1 class="text-6xl font-bold text-gray-900">Hello {User}</h1>
     <p class="mt-5 text-xl text-gray-600">{welcome}</p>
+    <p class="mt-5 text-xl text-gray-600 line-through">{downMessage}</p>
   </div>
 </section>
 
