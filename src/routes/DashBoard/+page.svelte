aff8f55f4d1ad94911edcaf91a351948a3f0257a
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-05 20:25:08 -0700

    Created a dashboard file
    Starting styling and creating layout

---
+++ src/routes/DashBoard/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import CardTracker from "../../lib/components/CardTracker.svelte";
+</script>
+
+<section class="dash-grid">
+    <div>
+        <CardTracker numberTracker="0" trackerDescription="Total de casos confirmados" />
+    </div>
+</section>
+
+
+<style>
+    .dash-grid{
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+    }
+</style>
