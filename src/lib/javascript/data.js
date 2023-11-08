9f539440f2b51298e535bc834b00d759a0463801
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-08 08:31:18 -0700

    Fixed the bug where the information would not render

--- src/lib/javascript/data.js
+++ src/lib/javascript/data.js
@@ -1,62 +1,62 @@
- export let candidateInfo = [{
-  candidate1: {
+ export let candidateInfo = [
+{
   name: "Rolls King",
   description: "Studies at MRU",
   skills: "C#, .NET Framework, Angular, Bootstrap",
   image: "/images/headshot-1.jpg"
   },
-  candidate2: {
+  {
   name: "Leeroy Bishop",
   description: "Studies at UOFC",
   skills: "React, Node.js, Express, MongoDB",
   image: "/images/headshot-2.jpg"
   },
-  candidate3: {
+{
     name: "Krishna Patel",
     description: "Studies at SAIT",
     skills: "Svelte, Vue.js, Firebase, Tailwind CSS",
     image: "/images/headshot-3.jpg"
   },
-  candidate4:{
+{
     name: "Allen Provolt",
     description: "Studies at NAIT",
     skills: "Java, Spring Boot, MySQL, Hibernate",
     image: "/images/headshot-4.jpg"
   },
-  candidate5:{
+{
     name: "Mustafa Al-Bassam",
     description: "Studies at King Abdualaziz University",
     skills: "React, Node.js, Angular, PostgreSQL",
     image: "/images/headshot-5.jpg"
   },
-  candidate6:{
+{
     name: "Samira Al-Mutairi",
     description: "Studies at King Abdualaziz University",
     skills: "Linux, Apache, MySQL, PHP",
     image: "/images/headshot-6.jpg"
   },
-  candidate7:{
+{
     name: "Elyanna Marijeh",
     description: "Studies at King Abdualaziz University",
     skills: "C#, .NET Framework, Angular, Bootstrap",
     image: "/images/headshot-7.jpg"
   },
-  candidate8:{
+{
     name: "Farah Al-Shehri",
     description: "Studies at Georgia Tech",
     skills: "Data Science, Python, R, SQL",
     image: "/images/headshot-8.jpg"
   },
-  candidate9:{
-    name: "Hari Dalogdog",
+{
+    name: "James Sarisag",
     description: "Studies at University of Toronto",
     skills: "C, C++, Java, Python",
     image: "/images/headshot-9.jpg"
   },
-  candidate10:{
+{
     name: "Manuela Matahom",
     description: "Studies at University of Alberta",
     skills: "C#, .NET Framework, Angular, Bootstrap",
     image: "/images/headshot-10.jpg"
-  },
-}]
+  }
+]
