466aff3d4f72a353c2c3f086e8803f2ba524ba26
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-08 08:01:52 -0700

    Made some default props

--- src/lib/components/CardTracker.svelte
+++ src/lib/components/CardTracker.svelte
@@ -1,20 +1,31 @@
 <script>
-  export let numberTracker = 0;
-  export let trackerDescription = "Descrição do tracker";
+  export let position = "Position";
+  export let recentPost = "0";
+  export let applicantCount = "0";
+  export let recentApplicants = "0";
+
+  $: recentPost = parseInt(recentPost);
+  $: applicantCount = parseInt(applicantCount);
+  $: recentApplicants = Math.floor(applicantCount / 7 + 5);
 </script>
+  
+  <div class="p-2 rounded-md cursor-pointer gradient w-80">
+    <div class="ml-4">
+      <h1 class="text-2xl text-white">{position}</h1>
+      <p class="text-sm white">Posted {recentPost} days ago</p>
+    </div>
+    <div class="mt-5 ml-5">
+      <span class="text-3xl">{applicantCount}</span>
+       <span class="text-white">Applicants</span> 
+       <span class="ml-10 text-sm text-blue-400">{recentApplicants} applied last week</span>
+    </div>
+  </div>
 
-<div class="trackerCard">
-    <h1>{numberTracker}</h1>
-    <p>{trackerDescription}</p>
-</div>
-
+  
 
 <style>
-  .trackerCard{
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0,0,0,0.2);
-    padding: 20px;
-    text-align: center;
+  
+  .gradient{
+    background: linear-gradient(90deg, rgb(15, 194, 39), rgb(37, 19, 141));
   }
 </style>
