fc8c0d9decdf6caec965e1bb8629f26945a6792e
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-08 08:04:34 -0700

    Changed the layout of dashboard page

--- src/routes/DashBoard/+page.svelte
+++ src/routes/DashBoard/+page.svelte
@@ -1,18 +1,32 @@
 <script>
     import CardTracker from "../../lib/components/CardTracker.svelte";
+    import SearchBar from "../../lib/components/SearchBar.svelte";
+    import Candidate from "../../lib/components/Candidate.svelte";
 </script>
 
-<section class="dash-grid">
-    <div>
-        <CardTracker numberTracker="0" trackerDescription="Total de casos confirmados" />
+<SearchBar currentPage="Dashboard" />
+<section>
+    <h1 class="text-3xl title">Overview</h1>
+    <div class="flex justify-evenly">
+        <CardTracker position="Sr. Web Developer" recentPost="10" applicantCount="65" />
+        <CardTracker position="UI/UX Designer" recentPost="30+" applicantCount="33"/>
+        <CardTracker position="Data Analyst" recentPost="30+" applicantCount="75"/>
+    </div>
+</section>
+
+<section>
+    <h1 class="text-3xl title">Candidates</h1>
+    <div class="flex flex-wrap gap-2 justify-evenly">
+        <Candidate {candidate1}/>
     </div>
 </section>
 
 
 <style>
-    .dash-grid{
-        display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        grid-template-rows: repeat(6, 1fr);
+
+    .title{
+        margin: 2rem;
+        cursor: default;
     }
+
 </style>
