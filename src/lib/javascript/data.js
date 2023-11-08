e8f27eb3bececf29f87a5959e9746c15c5745670
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-08 09:39:30 -0700

    Changed object array
    Object array turned into individual objects and passed into an array to be iterated through

--- src/lib/javascript/data.js
+++ src/lib/javascript/data.js
@@ -1,62 +1,85 @@
- export let candidateInfo = [
-{
+let candidate1 = {
   name: "Rolls King",
   description: "Studies at MRU",
   skills: "C#, .NET Framework, Angular, Bootstrap",
   image: "/images/headshot-1.jpg"
-  },
-  {
+};
+
+let candidate2 = {
   name: "Leeroy Bishop",
   description: "Studies at UOFC",
   skills: "React, Node.js, Express, MongoDB",
   image: "/images/headshot-2.jpg"
-  },
-{
-    name: "Krishna Patel",
-    description: "Studies at SAIT",
-    skills: "Svelte, Vue.js, Firebase, Tailwind CSS",
-    image: "/images/headshot-3.jpg"
-  },
-{
-    name: "Allen Provolt",
-    description: "Studies at NAIT",
-    skills: "Java, Spring Boot, MySQL, Hibernate",
-    image: "/images/headshot-4.jpg"
-  },
-{
-    name: "Mustafa Al-Bassam",
-    description: "Studies at King Abdualaziz University",
-    skills: "React, Node.js, Angular, PostgreSQL",
-    image: "/images/headshot-5.jpg"
-  },
-{
-    name: "Samira Al-Mutairi",
-    description: "Studies at King Abdualaziz University",
-    skills: "Linux, Apache, MySQL, PHP",
-    image: "/images/headshot-6.jpg"
-  },
-{
-    name: "Elyanna Marijeh",
-    description: "Studies at King Abdualaziz University",
-    skills: "C#, .NET Framework, Angular, Bootstrap",
-    image: "/images/headshot-7.jpg"
-  },
-{
-    name: "Farah Al-Shehri",
-    description: "Studies at Georgia Tech",
-    skills: "Data Science, Python, R, SQL",
-    image: "/images/headshot-8.jpg"
-  },
-{
-    name: "James Sarisag",
-    description: "Studies at University of Toronto",
-    skills: "C, C++, Java, Python",
-    image: "/images/headshot-9.jpg"
-  },
-{
-    name: "Manuela Matahom",
-    description: "Studies at University of Alberta",
-    skills: "C#, .NET Framework, Angular, Bootstrap",
-    image: "/images/headshot-10.jpg"
-  }
-]
+};
+
+let candidate3 = {
+  name: "Krishna Patel",
+  description: "Studies at SAIT",
+  skills: "Svelte, Vue.js, Firebase, Tailwind CSS",
+  image: "/images/headshot-3.jpg"
+};
+
+let candidate4 = {
+  name: "Allen Provolt",
+  description: "Studies at NAIT",
+  skills: "Java, Spring Boot, MySQL, Hibernate",
+  image: "/images/headshot-4.jpg"
+};
+
+let candidate5 = {
+  name: "Mustafa Al-Bassam",
+  description: "Studies at King Abdualaziz University",
+  skills: "React, Node.js, Angular, PostgreSQL",
+  image: "/images/headshot-5.jpg"
+};
+
+let candidate6 = {
+  name: "Samira Al-Mutairi",
+  description: "Studies at King Abdualaziz University",
+  skills: "Linux, Apache, MySQL, PHP",
+  image: "/images/headshot-6.jpg"
+};
+
+let candidate7 = {
+  name: "Elyanna Marijeh",
+  description: "Studies at King Abdualaziz University",
+  skills: "C#, .NET Framework, Angular, Bootstrap",
+  image: "/images/headshot-7.jpg"
+};
+
+let candidate8 = {
+  name: "Farah Al-Shehri",
+  description: "Studies at Georgia Tech",
+  skills: "Data Science, Python, R, SQL",
+  image: "/images/headshot-8.jpg"
+};
+
+let candidate9 = {
+  name: "James Sarisag",
+  description: "Studies at University of Toronto",
+  skills: "C, C++, Java, Python",
+  image: "/images/headshot-9.jpg"
+};
+
+let candidate10 = {
+  name: "Manuela Matahom",
+  description: "Studies at University of Alberta",
+  skills: "C#, .NET Framework, Angular, Bootstrap",
+  image: "/images/headshot-10.jpg"
+};
+
+let candidate11 = {
+  name: "Sahara Perez",
+  description: "Studies at University of British Columbia",
+  skills: "C#, .NET Framework, Angular, Bootstrap",
+  image: "/images/headshot-11.jpg"
+}
+
+let candidate12 = {
+  name: "Enrique Martinez",
+  description: "Studies at Harvard University",
+  skills: "C, C++, Java, Python, SQL",
+  image: "/images/headshot-12.jpg"
+}
+
+export let candidateInfo = [candidate1, candidate2, candidate3, candidate4, candidate5, candidate6, candidate7, candidate8, candidate9, candidate10, candidate11, candidate12];
