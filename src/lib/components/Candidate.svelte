ae049cdfb247d02252b610a42f32f6bb2dd07fbd
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-08 09:40:13 -0700

    Removed #each and added styles

--- src/lib/components/Candidate.svelte
+++ src/lib/components/Candidate.svelte
@@ -1,16 +1,15 @@
 <script>
-  import { candidateInfo } from "../javascript/data"
+  import { candidateInfo } from "../javascript/data";
+  export let candidate;
 </script>
 
-<div class="w-3/12 text-white transition duration-300 rounded-md bg-slate-600 hover:scale-95">
-  {#each candidateInfo as {name, description, skills, image}}
-  <div>
-    <img src={image} alt={name} />
-    <h1 class="text-center">{name}</h1>
-    <p class="text-center">{description}</p>
-    <div class="flex flex-wrap justify-center gap-2">
-      <div class="p-2 text-sm rounded-md bg-slate-700">{skills}</div>
-    </div>
-  </div>
+<div class="flex flex-col justify-center gap-2 p-2 text-white align-middle transition ease-linear rounded-lg delay-50 bg-slate-700 hover:scale-105">
+<img class="mx-auto rounded-md w-80" src={candidate.image} alt={candidate.name} />
+<h1 class="text-2xl text-center">{candidate.name}</h1>
+<p class="text-center">{candidate.description}</p>
+<div class="flex flex-wrap justify-center gap-2 cursor-pointer">
+  {#each candidate.skills.split(', ') as skill}
+    <div class="p-2 text-sm transition delay-75 rounded-md hover:bg-slate-600 bg-slate-900 hover:scale-110">{skill}</div>
   {/each}
+</div>
 </div>
